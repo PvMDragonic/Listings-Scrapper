939d54be9207628204b63718d2444cca31b0d4a9
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2025-04-01 09:42:18 -0300

    Clear input button.

--- frontend/src/components/searchBar.vue
+++ frontend/src/components/searchBar.vue
@@ -1,16 +1,28 @@
 <template>
-    <input 
-        @input = "onInput" 
-        v-model = "searchText"
-        placeholder = "Search term"
-        class = "input"
-    >
+    <div class = 'input'>
+        <input 
+            @input = "onInput" 
+            v-model = "searchText"
+            placeholder = "Search term"
+            class = "input__search"
+        >
+        <button 
+            v-if = "searchText !== ''"
+            @click = "handleSearchReset"
+            class = "input__clear"
+            title = "Clear search"
+            type = "button"
+        >
+            <ClearButton/>
+        </button>
+    </div>
 </template>
   
 <script setup lang = "ts">
     import { ref } from 'vue';
     import { api } from '../api/api';
     import type { ProcessedData } from '../App.vue';
+    import ClearButton from '../assets/clearButton.vue';
 
     const searchText = ref('');
     const debounceTimeout = ref<number | null>(null);
@@ -29,21 +41,27 @@
         );
     };
 
+    function handleSearchReset()
+    {
+        searchText.value = '';
+        emit('updateListings', []);
+    }
+
     async function makePostRequest() 
     {
         if (searchText.value.trim() === '')
-            emit('updateListings', []);
-        else
-            try 
-            {
-                const response = await api.post('/process-data', {
-                    searchTerm: searchText.value
-                });
-                emit('updateListings', response.data);
-            } 
-            catch (error) 
-            {
-                console.error('Error making POST request:', error);
-            }
+            return;
+
+        try 
+        {
+            const response = await api.post('/process-data', {
+                searchTerm: searchText.value
+            });
+            emit('updateListings', response.data);
+        } 
+        catch (error) 
+        {
+            console.error('Error making POST request:', error);
+        }
     }    
 </script>
